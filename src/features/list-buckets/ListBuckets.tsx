611eca2185d60f4727322bf62a7a59e5c8f32b4a
jer-nc <rejdev24@gmail.com>
2023-12-22 14:51:24 +0100

    refactor: ListBuckets component and use react-query for fetching bucket data

--- src/features/list-buckets/ListBuckets.tsx
+++ src/features/list-buckets/ListBuckets.tsx
@@ -1,64 +1,45 @@
 import { Button } from '@/components/ui/button';
 import { Plus, RefreshCcw } from 'lucide-react';
-import { useState, useEffect } from 'react';
-import { useToast } from '@/components/ui/use-toast';
+import { toast } from '@/components/ui/use-toast';
 import { listAllBuckets } from '@/cli-functions';
 import { useUserSessionStore } from '@/store/useSessionStore';
 import Spinner from '@/components/custom/loaders/Spinner';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Card } from '@/components/ui/card';
 import CardDropdown from '@/components/custom/dropdowns/CardDropdown';
 import bucketIcon from '@/assets/icons/bucket-icon.svg';
-import { useBucketStore } from '@/store/useBucketStore';
+import { useQuery } from '@tanstack/react-query';
+import { Bucket } from '@/lib/app';
 
 
 function ListBuckets() {
-    const { toast } = useToast();
+    const { profiles } = useUserSessionStore();
+    const { pathname: currentPathname } = useLocation();
     const navigate = useNavigate();
+    const profile = localStorage.getItem('aws-profile') || '';
+    const bucketName = currentPathname.replace('/buckets/', '');
 
-    const { setCurrentBucketRegion, setBuckets, buckets } = useBucketStore();
-    const { profiles, currentProfile } = useUserSessionStore();
-    const [loading, setLoading] = useState(false);
-    const [forceRefresh, setForceRefresh] = useState(false);
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ['buckets', profile, bucketName],
+        queryFn: () => listAllBuckets(profile),
+        retry: 1,
+    });
 
+    if (isError) {
+        toast({
+            title: 'Error',
+            description: error.message,
+            variant: 'destructive',
+            className: 'text-xs',
+        })
+    }
 
-    const handleListBuckets = async () => {
-        try {
-            setLoading(true);
-            const profile = localStorage.getItem('aws-profile') || '';
-            const bucketList = await listAllBuckets(profile);
-            console.log('bucketList', bucketList.Buckets)
-            setBuckets(bucketList.Buckets);
-        } catch (error) {
-            console.error(error);
-            setBuckets([]);
-            if (error instanceof Error) {
-                toast({
-                    title: 'Error',
-                    description: error.message,
-                    variant: 'destructive',
-                    className: 'text-xs',
-                });
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
+    console.log('data', data)
+
 
     const handleNavigate = (bucket: string) => {
         navigate(`/buckets/${bucket}`);
     };
-
-    useEffect(() => {
-        if (profiles.length > 0 && forceRefresh) {
-            handleListBuckets();
-        }
-    }, [currentProfile, profiles]);
-
-    useEffect(() => {
-        setCurrentBucketRegion('');
-        setForceRefresh(true)
-    }, [currentProfile])
 
 
     return (
@@ -66,12 +47,12 @@ function ListBuckets() {
             <div className='top-14 sticky w-full z-50  flex justify-between items-center bg-background py-4'>
                 <p className='font-semibold'>S3 Buckets</p>
 
-                <Button size='icon' variant='ghost' onClick={handleListBuckets}>
+                <Button size='icon' variant='ghost' >
                     <RefreshCcw size={18} />
                 </Button>
             </div>
-            <div className={`py-4 ${loading || profiles.length === 0 || buckets.length === 0 ? 'flex flex-col justify-center' : 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4'}`}>
-                {loading ? (
+            <div className={`py-4 ${isLoading || profiles.length === 0 || data?.Buckets.length === 0 ? 'flex flex-col justify-center' : 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4'}`}>
+                {isLoading ? (
                     <div style={{ height: 'calc(100vh - 14.5rem)' }} className='flex items-center justify-center py-8'>
                         <Spinner />
                     </div>
@@ -79,7 +60,7 @@ function ListBuckets() {
                     <div style={{ height: 'calc(100vh - 14.5rem)' }} className='mx-auto text-center flex flex-col justify-center'>
                         <p className='text-sm truncate mx-auto max-w-[10rem] text-muted-foreground'>No profiles available</p>
                     </div>
-                ) : buckets.length === 0 && !loading ? (
+                ) : (data as Array<unknown>)?.length === 0 && !isLoading ? (
                     <div style={{ height: 'calc(100vh - 14.5rem)' }} className='mx-auto text-center flex flex-col justify-center'>
                         <p className='text-sm truncate mx-auto max-w-[10rem] text-muted-foreground'>No buckets available</p>
                         <Button className='mt-4'>
@@ -88,7 +69,7 @@ function ListBuckets() {
                         </Button>
                     </div>
                 ) : (
-                    buckets.map((bucket, index) => (
+                    data?.Buckets.map((bucket: Bucket, index: number) => (
                         <Card onClick={() => handleNavigate(bucket.Name)} title={bucket.Name} key={index} className='p-4 flex items-center justify-between gap-4 hover:bg-secondary/30 rounded-md cursor-pointer'>
                             <div className='flex items-center gap-4'>
                                 <img src={bucketIcon} alt='bucket icon' className='w-5 h-5' />
