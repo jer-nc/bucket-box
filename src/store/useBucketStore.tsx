c9e6bdffbd2027f73da104277052b12798660480
jer-nc <rejdev24@gmail.com>
2023-12-22 15:03:41 +0100

    Remove unused code related to bucket and subfolder contents fetching

--- src/store/useBucketStore.tsx
+++ src/store/useBucketStore.tsx
@@ -1,17 +1,12 @@
-import { Bucket } from '@/lib/app';
 import { create } from 'zustand';
 
 
 interface BucketStore {
-    buckets: Bucket[];
-    setBuckets: (buckets: Bucket[]) => void;
     currentBucketRegion: string;
     setCurrentBucketRegion: (region: string) => void;
 }
 
 export const useBucketStore = create<BucketStore>((set) => ({
-    buckets: [],
-    setBuckets: (buckets) => set({ buckets }),
     currentBucketRegion: '',
     setCurrentBucketRegion: (region) => set({ currentBucketRegion: region }),
 }));
